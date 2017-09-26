42bf158395acfe8e4eda0ebfa91959c7baec9a31
Alexander Kravin <krsasha2004@gmail.com>
2017-09-26 14:32:17 +0300

    Move src/main/frontend to frontend

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,16 +1,16 @@
 var path = require('path');
 
 module.exports = {
-    entry: './src/main/frontend/index.js',
+    entry: './app/index.js',
     output: {
-        path: path.resolve(__dirname, "src/main/resources/static/build"),
+        path: path.resolve(__dirname, "../src/main/resources/static/build"),
         publicPath: '/build',
         filename: 'bundle.js'
     },
 
     devServer: {
 //        publicPath: "/",
-        contentBase: "./src/main/resources/static",
+        contentBase: "../src/main/resources/static",
         //hot: true,
 //        inline: true,
         port: 8080
@@ -22,7 +22,7 @@ module.exports = {
                 test: /\.js$/,
                 exclude: /node_modules/,
                 include: [
-                    path.resolve(__dirname, "src/main/frontend")
+                    path.resolve(__dirname, "app")
                 ],
 
                 loader: "babel-loader"
